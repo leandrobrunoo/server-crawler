6e596da62934b3d2e4a634016393712ec15210aa
Leandro Bruno <leandrobrunoo@msn.com>
2018-08-12 19:35:01 -0300

    Ajuste pra subir pra o umbler

--- index.js
+++ index.js
@@ -7,54 +7,6 @@ app = express();
 
 app.get('/placares', function (req, res) {
 
-        let scrape = async () => {
-            const browser = await puppeteer.launch({
-                headless: true,
-                args: [
-                    '--no-sandbox',
-                    '--disable-setuid-sandbox'
-                ]
-            });
-
-            const page = await browser.newPage();
-            await page.goto('https://play.livebet.com/#/results/?lang=pt-br');
-            await page.waitFor(15000);
-
-            const result = await page.evaluate(() => {
-
-                let data = [];
-                let elements = [...document.querySelectorAll('body > div.body-wrapper.lang-pt-br.results.theme-.livebet.playlivebetcom.classic.footer-movable > div.view-container.ng-scope.results > ng-include > div > div.center-container-p > div > div.results-table-j > table > tbody > tr')]; // Select all Products
-                elements.shift();
-
-                for (var element of elements) {
-                    if (element.innerText != 'No information available' && element.innerText != 'Void') {
-                        let partida = {};
-                        partida.data = element.innerText.split('\n')[0];
-                        partida.campeonato = element.innerText.split('\n')[1];
-                        partida.timeCasa = element.innerText.split('\n')[2].split(' - ')[0];
-                        partida.timeFora = element.innerText.split('\n')[2].split(' - ')[1];
-                        partida.primeiroTempo = element.innerText.split('\n')[3].split(' ')[1];
-                        partida.placarFinal = element.innerText.split('\n')[3].split(' ')[0];
-                        data.push(partida);
-                    }
-                }
-                return data;
-            });
-            browser.close();
-            return result;
-        };
-
-        scrape().then((value) => {
-            // console.log(value);
-            res.send(value);
-        }).catch(e => {
-            res.send(e);
-        });
-
-});
-
-app.get('/raspagem', function (req, res) {
-
     let scrape = async () => {
         const browser = await puppeteer.launch({
             headless: true,
@@ -99,6 +51,18 @@ app.get('/raspagem', function (req, res) {
         res.send(e);
     });
 
+});
+
+app.get('/', function (req, res) {
+    res.send({
+        "name": "server-crawler",
+        "version": "1.0.0",
+        "developer": "Leandro Bruno Teixeira",
+        "endpoint": {
+            "GET": "/placares"
+        }
+    }
+    );
 });
 
 // Execução do serviço
