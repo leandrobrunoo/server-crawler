717cd6d9ae1fd65950b1ed1725677310193b664d
Leandro Bruno <leandrobrunoo@msn.com>
2018-11-07 03:17:17 -0300

    ajustes

--- index.js
+++ index.js
@@ -44,12 +44,12 @@ app.get('/placares', function (req, res) {
                 for (var element of elements) {
                     if (element.innerText != 'No information available' && element.innerText != 'Void') {
                         let partida = {};
-                        partida.dados = element.innerText;
+                    //  partida.dados = element.innerText;
                         partida.data = element.innerText.split('\n\t\n')[0];
                         partida.campeonato = element.innerText.split('\n\t\n')[1];
                         partida.timeCasa = element.innerText.split('\n\t\n')[2].split(' - ')[0];
                         partida.timeFora = element.innerText.split('\n\t\n')[2].split(' - ')[1];
-                        partida.primeiroTempo = element.innerText.split('\n\t\n')[3].split(' ')[1];
+                        partida.primeiroTempo = element.innerText.split('\n\t\n')[3].split(' ')[1].replace('\n\n','');
                         partida.placarFinal = element.innerText.split('\n\t\n')[3].split(' ')[0];
                         data.push(partida);
                     }
@@ -93,12 +93,12 @@ app.get('/raspagem', function (req, res) {
             for (var element of elements) {
                 if (element.innerText != 'No information available' && element.innerText != 'Void') {
                     let partida = {};
-                    partida.data = element.innerText.split('\n')[0];
-                    partida.campeonato = element.innerText.split('\n')[1];
-                    partida.timeCasa = element.innerText.split('\n')[2].split(' - ')[0];
-                    partida.timeFora = element.innerText.split('\n')[2].split(' - ')[1];
-                    partida.primeiroTempo = element.innerText.split('\n')[3].split(' ')[1];
-                    partida.placarFinal = element.innerText.split('\n')[3].split(' ')[0];
+                    partida.data = element.innerText.split('\n\t\n')[0];
+                    partida.campeonato = element.innerText.split('\n\t\n')[1];
+                    partida.timeCasa = element.innerText.split('\n\t\n')[2].split(' - ')[0];
+                    partida.timeFora = element.innerText.split('\n\t\n')[2].split(' - ')[1];
+                    partida.primeiroTempo = element.innerText.split('\n\t\n')[3].split(' ')[1].replace('\n\n','');
+                    partida.placarFinal = element.innerText.split('\n\t\n')[3].split(' ')[0];
                     data.push(partida);
                 }
             }
