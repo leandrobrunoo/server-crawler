850fa3f27bd6aa3262924e4abbe952fbc5101536
Leandro Bruno <leandrobrunoo@msn.com>
2018-11-11 17:46:53 -0300

    Ajustes

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ app.get('/placares', function (req, res) {
                 '--disable-setuid-sandbox'
             ]
         },
-       /* {
+        /*{
             executablePath: 'C:/Users/Leandro/AppData/Local/Google/Chrome/Application'
         }*/
         );
 
         const page = await browser.newPage();
-        await page.goto('https://play.livebet.com/#/results/?lang=pt-br');
+        await page.goto('https://play.livebet.com/#/results/?lang=pt-br', { waitUntil : ['load', 'domcontentloaded']});
         await page.waitFor(15000);
-
+ 
         const result = await page.evaluate(() => {
 
             let data = [];
@@ -40,7 +40,10 @@ app.get('/placares', function (req, res) {
                     partida.timeFora = element.innerText.split('\n')[2].split(' - ')[1];
                     partida.primeiroTempo = element.innerText.split('\n')[3].split(' ')[1];
                     partida.placarFinal = element.innerText.split('\n')[3].split(' ')[0];
-                    data.push(partida);
+
+                    if(partida.placarFinal != 'Void'){
+                        data.push(partida);
+                    }
                 }
             }
             return data;
@@ -68,6 +71,74 @@ app.get('/', function (req, res) {
         }
     }
     );
+});
+
+app.get('/teste', function (req, res) {
+
+    let scrape = async () => {
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox'
+            ]
+        },
+        /*{
+            executablePath: 'C:/Users/Leandro/AppData/Local/Google/Chrome/Application'
+        }*/
+        );
+
+        const page = await browser.newPage();
+        await page.goto('https://play.livebet.com/#/results/?lang=pt-br', { waitUntil : ['load', 'domcontentloaded']});
+        await page.waitFor(10000);
+ 
+        const selectorData = 'body > div.body-wrapper.lang-pt-br.results.theme-.livebet.playlivebetcom.classic.footer-movable > div.view-container.ng-scope.results > ng-include > div > div.center-container-p > div > div.navigation-of-results-j > div:nth-child(1) > ul > li:nth-child(3) > div > div > div';
+        await page.waitForSelector(selectorData);
+        await page.click(selectorData);
+        await page.keyboard.press('ArrowLeft');
+        await page.keyboard.press('Enter');
+        await page.waitFor(5000);
+        
+        const selectorPesquisar = 'body > div.body-wrapper.lang-pt-br.results.theme-.livebet.playlivebetcom.classic.footer-movable > div.view-container.ng-scope.results > ng-include > div > div.center-container-p > div > div.navigation-of-results-j > div.results-table-cell-j.button-container-j > button';
+        await page.waitForSelector(selectorPesquisar);
+        await page.click(selectorPesquisar);
+        await page.waitFor(10000);
+
+        const result = await page.evaluate(() => {
+
+            let data = [];
+            let elements = [...document.querySelectorAll('body > div.body-wrapper.lang-pt-br.results.theme-.livebet.playlivebetcom.classic.footer-movable > div.view-container.ng-scope.results > ng-include > div > div.center-container-p > div > div.results-table-j > table > tbody > tr')]; // Select all Products
+            
+            elements.shift();
+
+            for (var element of elements) {
+                if (element.innerText != 'No information available' && element.innerText != 'Void') {
+                    let partida = {};
+                    partida.data = element.innerText.split('\n')[0];
+                    partida.campeonato = element.innerText.split('\n')[1];
+                    partida.timeCasa = element.innerText.split('\n')[2].split(' - ')[0];
+                    partida.timeFora = element.innerText.split('\n')[2].split(' - ')[1];
+                    partida.primeiroTempo = element.innerText.split('\n')[3].split(' ')[1];
+                    partida.placarFinal = element.innerText.split('\n')[3].split(' ')[0];
+
+                    if(partida.placarFinal != 'Void'){
+                        data.push(partida);
+                    }
+                }
+            }
+            return data;
+        });
+        browser.close();
+        return result;
+    };
+
+    scrape().then((value) => {
+        // console.log(value);
+        res.send(value);
+    }).catch(e => {
+        res.send(e);
+    });
+
 });
 
 // Execução do serviço
