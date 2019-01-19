fcf8735f75f362b5e618c1ac2cb469aed16bcbbf
Leandro Bruno <leandrobrunoo@msn.com>
2019-01-19 20:15:36 -0300

    Mudanças nas tags do html do site

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const PORT = process.env.PORT || 5000;
 
 app = express();
-app.get('/placares', function (req, res) {
+app.get('/teste', function (req, res) {
 
     let scrape = async () => {
         const browser = await puppeteer.launch({
@@ -22,12 +22,12 @@ app.get('/placares', function (req, res) {
         const page = await browser.newPage();
     //    await page.setUserAgent('5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36');
         await page.goto('https://play.livebet.com/#/results/?lang=pt-br', { waitUntil : ['load', 'domcontentloaded']});
-        await page.waitFor(15000);
+        await page.waitFor(20000);
  
         const result = await page.evaluate(() => {
 
             let data = [];
-            let elements = [...document.querySelectorAll('body > div.body-wrapper.lang-pt-br.results.theme-.livebet.playlivebetcom.classic.footer-movable > div.view-container.ng-scope.results > ng-include > div > div.center-container-p > div > div.results-table-j > table > tbody > tr')]; // Select all Products
+            let elements = [...document.querySelectorAll('body > div.body-wrapper.lang-pt-br.results.theme-.livebet.playlivebetcom.classic.footer-movable > div.view-container.results > ng-include > div > div.center-container-p > div > div.results-table-j > table > tbody > tr')]; // Select all Products
             
             elements.shift();
 
@@ -64,7 +64,7 @@ app.get('/placares', function (req, res) {
 app.get('/', function (req, res) {
     res.send({
         "name": "server-crawler",
-        "version": "1.0.0",
+        "version": "1.1.0",
         "developer": "Leandro Bruno Teixeira",
         "endpoint": {
             "GET": "/placares"
@@ -73,7 +73,7 @@ app.get('/', function (req, res) {
     );
 });
 
-app.get('/teste', function (req, res) {
+app.get('/placares', function (req, res) {
 
     let scrape = async () => {
         const browser = await puppeteer.launch({
@@ -90,36 +90,41 @@ app.get('/teste', function (req, res) {
 
         const page = await browser.newPage();
         await page.goto('https://play.livebet.com/#/results/?lang=pt-br', { waitUntil : ['load', 'domcontentloaded']});
-        await page.waitFor(10000);
- 
-        const selectorData = 'body > div.body-wrapper.lang-pt-br.results.theme-.livebet.playlivebetcom.classic.footer-movable > div.view-container.ng-scope.results > ng-include > div > div.center-container-p > div > div.navigation-of-results-j > div:nth-child(1) > ul > li:nth-child(3) > div > div > div';
+        await page.waitFor(15000);
+        console.log('Site carregado!');
+        
+        const selectorData = 'body > div.body-wrapper.lang-pt-br.results.theme-.livebet.playlivebetcom.classic.footer-movable > div.view-container.results > ng-include > div > div.center-container-p > div > div.navigation-of-results-j > div:nth-child(1) > ul > li:nth-child(3) > div > div > div > input';
         await page.waitForSelector(selectorData);
+        console.log('Agora vou clicar!');
         await page.click(selectorData);
         await page.keyboard.press('ArrowLeft');
         await page.keyboard.press('Enter');
         await page.waitFor(5000);
         
-        const selectorPesquisar = 'body > div.body-wrapper.lang-pt-br.results.theme-.livebet.playlivebetcom.classic.footer-movable > div.view-container.ng-scope.results > ng-include > div > div.center-container-p > div > div.navigation-of-results-j > div.results-table-cell-j.button-container-j > button';
+        const selectorPesquisar = 'body > div.body-wrapper.lang-pt-br.results.theme-.livebet.playlivebetcom.classic.footer-movable > div.view-container.results > ng-include > div > div.center-container-p > div > div.navigation-of-results-j > div.results-table-cell-j.button-container-j > button';
         await page.waitForSelector(selectorPesquisar);
+        console.log('Agora vou clicar na pesquisa!');
         await page.click(selectorPesquisar);
         await page.waitFor(10000);
 
         const result = await page.evaluate(() => {
 
             let data = [];
-            let elements = [...document.querySelectorAll('body > div.body-wrapper.lang-pt-br.results.theme-.livebet.playlivebetcom.classic.footer-movable > div.view-container.ng-scope.results > ng-include > div > div.center-container-p > div > div.results-table-j > table > tbody > tr')]; // Select all Products
+            let elements = [...document.querySelectorAll('body > div.body-wrapper.lang-pt-br.results.theme-.livebet.playlivebetcom.classic.footer-movable > div.view-container.results > ng-include > div > div.center-container-p > div > div.results-table-j > table > tbody > tr')]; // Select all Products
+                                
+            console.log(elements.innerText);
             
             elements.shift();
 
             for (var element of elements) {
                 if (element.innerText != 'No information available' && element.innerText != 'Void') {
                     let partida = {};
-                    partida.data = element.innerText.split('\n')[0];
-                    partida.campeonato = element.innerText.split('\n')[1];
-                    partida.timeCasa = element.innerText.split('\n')[2].split(' - ')[0];
-                    partida.timeFora = element.innerText.split('\n')[2].split(' - ')[1];
-                    partida.primeiroTempo = element.innerText.split('\n')[3].split(' ')[1];
-                    partida.placarFinal = element.innerText.split('\n')[3].split(' ')[0];
+                    partida.data = element.innerText.split('\t')[0].replace(/(\r\n|\n|\r)/gm, '');
+                    partida.campeonato = element.innerText.split('\t')[1].replace(/(\r\n|\n|\r)/gm, '');
+                    partida.timeCasa = element.innerText.split('\t')[2].split(' - ')[0].replace(/(\r\n|\n|\r)/gm, '');
+                    partida.timeFora = element.innerText.split('\t')[2].split(' - ')[1].replace(/(\r\n|\n|\r)/gm, '');
+                    partida.primeiroTempo = element.innerText.replace(/(\r\n|\n|\r)/gm, '').split('\t')[3].split(' ')[1];
+                    partida.placarFinal = element.innerText.split('\t')[3].split(' ')[0].replace(/(\r\n|\n|\r)/gm, '');
 
                     if(partida.placarFinal != 'Void'){
                         data.push(partida);
