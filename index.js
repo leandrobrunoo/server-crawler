9ecbabfdd4335da075da5fde6eb2962616c98dd6
Leandro Bruno <leandrobrunoo@msn.com>
2019-09-15 22:02:42 -0300

    ajustes endpoint livescore

--- index.js
+++ index.js
@@ -233,7 +233,7 @@ app.get('/livescore', function (req, res) {
 
     let scrape = async () => {
         const browser = await puppeteer.launch({
-            headless: false,
+            headless: true,
             args: [
                 '--no-sandbox',
                 '--disable-setuid-sandbox'
